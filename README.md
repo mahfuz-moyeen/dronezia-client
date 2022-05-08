12871aac67d2c611867e5621c38f7228c08cd471
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-08 11:54:31 +0600

    add profile

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 ### drone warhouse client side.
 <hr/>
 
-## Live site : [Dronezia]()
+## Live site : [Dronezia](https://dronezia.web.app/)
+
+## Description : 
+This is a single-page web-application. create by `React.js`, login auth by `Firebase`. It is a Drone war-house website .You can add your item , update your item, delete your item. you can also control item stock . you can add your item and show your add all item in my item page.
+
+## UI / UX
+* Home page : banner, 6 item, our service, send message and footer.  
+  inventory item : show how many items we have and show All items
+* Manage Inventory : Show how many items we have and a table to show all items . here show 
+  2 button one edit your item and other delete your item.
+* single item page: show item all details and have 2 button one update stock or add stock item by 
+  input field other button delivery item and your item stock -1 and one edit button to edit your full item details.
+* Add item : show a form box for type to add your item details and click add item
+* profile icon: here have 3 button 1.profile details 2.my item 3.logout
 
 ## Use Items 
 
--- src/components/Share/UserProfile/UserProfile.js
+++ src/components/Share/UserProfile/UserProfile.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const UserProfile = () => {
+    const [user] = useAuthState(auth)
+
     return (
-        <div>
-            <h1>user Profile</h1>
+        <div className='card w-96 bg-base-100 shadow-xl mx-auto my-10'>
+            <div className='card-body mx-auto'>
+                <div className='flex justify-center scale-150 my-10'>
+                    <div className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-indigo-600"
+
+                    >
+                        {
+                            user?.photoURL ?
+                                <img
+                                    src={user.photoURL}
+                                    className="w-full h-full rounded-full"
+                                    alt='userPhoto'
+                                    referrerPolicy="no-referrer"
+                                />
+                                :
+                                <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full ">
+                                    <svg className="absolute w-12 12 text-indigo-600 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+                                </div>
+                        }
+                    </div>
+                </div>
+                <div className="text-center">
+                    <h1 className="text-2xl mb-5">{user?.displayName}</h1>
+                    <h1 className="text-xl text-gray-500">{user?.email}</h1>
+                </div>
+            </div>
         </div>
     );
 };
